7d39685f7c794b37b9c7b6b672589904165c6ea9
Hitesh Rathod <hitesh.wappnet@gmail.com>
2024-03-13 12:01:12 +0530

    v3

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! From Plesk : v2';
+    return 'Hello World! From Plesk : v3';
   }
 }
